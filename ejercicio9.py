fc424ad7ae9a4e67382ae2ca1221b117af698e3a
Juan Vidal <juanmvg2003@gmail.com>
2024-02-06 18:45:24 -0500

    ejercicio 9

--- ejercicio9.py
+++ ejercicio9.py
@@ -8,47 +8,49 @@ Rango de Edad Fase
 >=18 y <30 Fase 5
 <18 En espera de Autorización
 """
-
+# libreria para limpriar comsola
+import os
 # aqui se van a almacenar las edades
 edades = []
 # funcion que recolecta las edades
 def recaudar_edades():
     # va a repetir la información 5 veces
     for i in range(1, 5+1):
-        # input del usuario
+        # nombre del usuario
+        nombre = input("Ingresa el nombre de la persona: ")
+        # edad del usuario
         usuario_edad = int(input(f"Ingresa la edad de la persona No.{i}: "))
-        # añadir la edad al array
-        edades.append(usuario_edad)
+        # añadir la edad al array y el número de la persona al array, generando tambien un array
+        edades.append(
+            {
+                "id_persona": i,
+                "Nombre_persona": nombre,
+                "edad": usuario_edad
+            }
+        )
     
-    print(f"Las edades ingresadas fueron: {", ".join(map(str, edades))}") 
-    
-    
-# funcion que obtiene el numero mayor
-def num_mayor(arr):
-    # inicializando la comparación
-    num_mayor = 0
-    # recorriendo el array de edades para saber el numero mayor
-    for numero in arr:
-        # cambiando el número mayor 
-        if(numero > num_mayor):
-            num_mayor = numero
-    # retorna el número mayor
-    return num_mayor
+    # limpiando la terminal
+    os.system("clear")
+    # recorriendo os diccionarios para comparar las edades
+    for diccionario in edades:
+        # fase 1
+        if(diccionario["edad"] >= 80):
+            print(f"Nombre: {diccionario["Nombre_persona"]}, Edad: {diccionario["edad"]}, Fase: Fase de vacunacion Fase 1")
+            # fase 2
+        elif(diccionario["edad"] >= 70 and diccionario["edad"] < 80):
+            print(f"Nombre: {diccionario["Nombre_persona"]}, Edad: {diccionario["edad"]}, Fase: Fase de vacunacion Fase 2")
+            # fase 3
+        elif(diccionario["edad"] >= 60 and diccionario["edad"] < 70):
+            print(f"Nombre: {diccionario["Nombre_persona"]}, Edad: {diccionario["edad"]}, Fase: Fase de vacunacion Fase 3")
+            # fase 4
+        elif(diccionario["edad"] >= 30 and diccionario["edad"] < 60):
+            print(f"Nombre: {diccionario["Nombre_persona"]}, Edad: {diccionario["edad"]}, Fase: Fase de vacunacion Fase 4")
+            # fase 5
+        elif(diccionario["edad"] >= 18 and diccionario["edad"] < 30):
+            print(f"Nombre: {diccionario["Nombre_persona"]}, Edad: {diccionario["edad"]}, Fase: Fase de vacunacion Fase 5")
+            # En espera de Autorización
+        else:
+            print(f"Nombre: {diccionario["Nombre_persona"]}, Edad: {diccionario["edad"]}, Fase: En espera de autorizacion.")
+            
 
-# funcion que obtiene el número menor
-def num_menor(arr):
-    # obteniendo el número mayor
-    num_menor = num_mayor(arr)
-    # recorriendo el array de edades para saber el numero menor
-    for numero in arr:
-        # comparando los numeros del array con el numero mayo
-        if(numero < num_menor):
-            num_menor = numero
-    # retorna el número mayor
-    return num_menor
-
-
-# llamado de las funciones
 recaudar_edades()
-print(f"El número mayor es: {num_mayor(edades)}")
-print(f"El número menor es:{num_menor(edades)}")
